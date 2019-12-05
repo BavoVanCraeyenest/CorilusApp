f825cedaad08b1527e7b372c02dbfe7831f4e653
bavo.vancraeyenest <bavo.vancraeyenest@student.odisee.be>
2019-12-05 11:01:38 +0100

    Assignment 1 and 2 finished

---
+++ CorilusApp/Controllers/DivisibleByThreeOrEightController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorilusApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DivisibleByThreeOrEightController : ControllerBase
+    {
+        // GET: api/DivisibleByThreeOrEight
+        [HttpGet]
+        public IActionResult Get(int value)
+        {
+            try
+            {
+                if (value % 3 == 0 && value % 8 == 0)
+                {
+                    return Ok("Het getal is deelbaar door 3 en door 8");
+                }
+                else if (value % 3 == 0)
+                {
+                    return Ok("Het getal is deelbaar door 3");
+                }
+                else if (value % 8 == 0)
+                {
+                    return Ok("Het getal is deelbaar door 8");
+                }
+                return Ok("Het getal is niet deelbaar door 3 of 8");
+            } catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ CorilusApp/Controllers/DivisibleController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorilusApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DivisibleController : ControllerBase
+    {
+        // GET: api/Divisible
+        [HttpGet]
+        public IActionResult Get(int value1, int value2)
+        {
+            try
+            {
+                if (value1 % value2 == 0)
+                {
+                    return Ok(true);
+                }
+                else
+                {
+                    return Ok(false);
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
